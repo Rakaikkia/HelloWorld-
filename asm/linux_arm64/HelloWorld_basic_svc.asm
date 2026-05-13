a906b36f1a9e76fe1fd80eaa75c111289b1814d7
qwxd2347zxc <ben19221986@gmail.com>
2026-05-14 01:41:04 +0200

    Asm ARM64

---
+++ asm/linux_arm64/HelloWorld_basic_svc.asm
@@ -0,0 +1,15 @@
+.data
+    msg:    .ascii "Hello, World!\n"
+    msg_len = . - msg
+.text
+    .global main
+main:
+    mov x8, #64
+    mov x0, #1
+    adrp x1, msg
+    add x1, x1, :lo12:msg
+    mov x2, msg_len
+    svc #0
+    mov x8, #93
+    mov x0, #0
+    svc #0
